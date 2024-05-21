7c197ad111acdfbf85969e341f9108ed6b947a7c
huy <thanhhuy9b@gmail.com>
2024-05-21 16:42:40 +0700

    xoa index and chinh link gg

--- controller/config.php
+++ controller/config.php
@@ -1,10 +1,10 @@
 <?php
 require_once "google-api/vendor/autoload.php";
 $gClient = new Google_Client();
-$gClient->setClientId("988343323625-u739gm7n5p5bh923aivna4fitp7ep525.apps.googleusercontent.com");
-$gClient->setClientSecret("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");
+$gClient->setClientId("741178376328-e2spkd02l8adi2f41b20b4am9j53fkhj.apps.googleusercontent.com");
+$gClient->setClientSecret("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");
 $gClient->setApplicationName("Vicode Media Login");
-$gClient->setRedirectUri("http://localhost/SHOPHOA_NHOM05/controller/controller.php");
+$gClient->setRedirectUri("http://localhost/QL_SHOPHOA/controller/controller.php");
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 // login URL
 $login_url = $gClient->createAuthUrl();
