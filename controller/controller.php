c7e542b8a6e6cbe832de8657e0f1f485bd7a0e2e
duongbaduy <riox.doubled@gmail.com>
2024-05-21 15:36:54 +0700

    Them controller google

---
+++ controller/controller.php
@@ -0,0 +1,28 @@
+<?php
+require_once "core/controller.Class.php";
+require_once "config.php";
+
+if (isset($_GET['code'])) {
+    $token = $gClient->fetchAccessTokenWithAuthCode($_GET['code']);
+}else{
+    header('Location: 123.php');
+    exit();
+}
+if(isset($token["error"]) != "invalid_grant"){
+    // get data from google
+    $oAuth = new Google_Service_Oauth2($gClient);
+    $userData = $oAuth->userinfo_v2_me->get();
+
+    //insert data in the database
+    $Controller = new Controller;
+    echo $Controller -> insertData(
+        array(
+            'email' => $userData['email'],
+            'avatar' => $userData['picture'],
+            'picture' => $userData['picture'],
+            'familyName' => $userData['familyName'],
+            'givenName' => $userData['givenName'],
+        )
+    );
+}
+?>
